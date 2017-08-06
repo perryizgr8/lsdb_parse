45cca0c6fe6066535a86372c28ea894f03fb0f3e
perryizgr8 <perryizgr8@live.in>
2017-08-06 18:02:21 +0530

    all lsas parsed

--- Lsa.py
+++ Lsa.py
@@ -14,13 +14,13 @@ class Lsa:
         self.adv_rtr = adv_rtr
         self.seq = seq
 
-        if ls_type is '0x01':
+        if ls_type == '0x01':
             Lsa.num_router_lsas += 1
-        elif ls_type is '0x02':
+        elif ls_type == '0x02':
             Lsa.num_network_lsas += 1
-        elif ls_type is '0x03':
+        elif ls_type == '0x03':
             Lsa.num_type3_lsas += 1
-        elif ls_type is '0x04':
+        elif ls_type == '0x04':
             Lsa.num_type4_lsas += 1
-        elif ls_type is '0x05':
+        elif ls_type == '0x05':
             Lsa.num_external_lsas += 1
--- lsa_utils.py
+++ lsa_utils.py
@@ -1,13 +1,97 @@
 #TODO write logics for these
 
-def get_lsa_age(first_byte, second_byte):
+def zero():
     return 0
+
+def one():
+    return 1
+
+def two():
+    return 2
+
+def three():
+    return 3
+
+def four():
+    return 4
+
+def five():
+    return 5
+
+def six():
+    return 6
+
+def seven():
+    return 7
+
+def eight():
+    return 8
+
+def nine():
+    return 9
+
+def ten():
+    return 10
+
+def eleven():
+    return 11
+
+def twelve():
+    return 12
+
+def thirteen():
+    return 13
+
+def fourteen():
+    return 14
+
+def fifteen():
+    return 15
+
+def get_int_from_hex(hex_byte):
+    options = {'0' : zero,
+               '1' : one,
+               '2' : two,
+               '3' : three,
+               '4' : four,
+               '5' : five,
+               '6' : six,
+               '7' : seven,
+               '8' : eight,
+               '9' : nine,
+               'A' : ten,
+               'B' : eleven,
+               'C' : twelve,
+               'D' : thirteen,
+               'E' : fourteen,
+               'F' : fifteen,
+               }
+    first_digit = options[hex_byte[2]]()
+    second_digit = options[hex_byte[3]]()
+    return first_digit*16 + second_digit
+
+def get_lsa_age(first_byte, second_byte):
+    first_int = get_int_from_hex(first_byte)
+    second_int = get_int_from_hex(second_byte)
+    return first_int*256 + second_int
 
 def get_lsid(first_byte, second_byte, third_byte, fourth_byte):
-    return 0
+    first_octet = str(get_int_from_hex(first_byte))
+    second_octet = str(get_int_from_hex(second_byte))
+    third_octet = str(get_int_from_hex(third_byte))
+    fourth_octet = str(get_int_from_hex(fourth_byte))
+    return first_octet + '.' + second_octet + '.' + third_octet + '.' + fourth_octet
 
 def get_rtr(first_byte, second_byte, third_byte, fourth_byte):
-    return 0
+    first_octet = str(get_int_from_hex(first_byte))
+    second_octet = str(get_int_from_hex(second_byte))
+    third_octet = str(get_int_from_hex(third_byte))
+    fourth_octet = str(get_int_from_hex(fourth_byte))
+    return first_octet + '.' + second_octet + '.' + third_octet + '.' + fourth_octet
 
 def get_seq(first_byte, second_byte, third_byte, fourth_byte):
-    return 0
+    first_int = get_int_from_hex(first_byte)
+    second_int = get_int_from_hex(second_byte)
+    third_int = get_int_from_hex(third_byte)
+    fourth_int = get_int_from_hex(fourth_byte)
+    return first_int*16777216 + second_int*65536 + third_int*256 + fourth_int
